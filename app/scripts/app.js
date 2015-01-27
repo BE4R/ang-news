c2da6365ba2e0b064211edbe2e40e3a2f2d432f1
James Jung <jamesofjungle@gmail.com>
2015-01-26 21:49:30 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,59 @@
+/* global app: true */
+/* exported app */
+
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular.module('angNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ])
+  .constant('FIREBASE_URL', 'https://ang-news-tuts.firebaseio.com/')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/post.html',
+        controller: 'PostCtrl'
+      })
+      .when('/posts/:postId', {
+        templateUrl: 'views/showpost.html',
+        controller: 'PostViewCtrl'
+      })
+      .when('/users/:userId', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl',
+        resolve: {
+          user: function(Auth) {
+            return Auth.resolveUser();
+          }
+        }
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl',
+        resolve: {
+          user: function(Auth) {
+            return Auth.resolveUser();
+          }
+        }
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/posts.js
@@ -0,0 +1,11 @@
+'use strict';
+
+app.controller('PostCtrl',function($scope, $location, Post, Auth){
+	$scope.posts = Post.all;
+	$scope.user = Auth.user;
+	$scope.post = {url: 'http://', title: ''};
+
+	$scope.deletePost = function(post) {
+		Post.delete(post);
+	};
+});
